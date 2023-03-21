0c12c822ec0c642b2b017b0a43655d02f35a5453
smurkel <mart_last@hotmail.com>
2023-03-21 23:26:57 +0100

    cmparing pyGPUreg templated, pyGPUreg paired, and stackreg.

--- tests.py
+++ tests.py
@@ -14,29 +14,46 @@ for SIZE in sizes:
     reg.init(image_size=SIZE)
 
     data = tifffile.imread("C:/Users/mart_/Desktop/pygpureg/2048.tif")[:, :SIZE, :SIZE]
-    n_frames = data.shape[0]
+    n_frames = 100#data.shape[0]
+
+
+    timer = time.time_ns()
+    for i in range(n_frames):
+        img, xy = reg.register(data[0], data[i])
+    pygpureg_paired_time = n_frames / ((time.time_ns() - timer) * 1e-9)  # fps
+
     ## pyGPUreg
     reg.set_template(data[0])
     timer = time.time_ns()
     for i in range(n_frames):
         img, xy = reg.register_to_template(data[i])
-    pygpureg_time = n_frames / ((time.time_ns() - timer) * 1e-9)  # fps
+    pygpureg_templated_time = n_frames / ((time.time_ns() - timer) * 1e-9)  # fps
 
     ## stackreg
-
     sr = StackReg(StackReg.TRANSLATION)
     timer = time.time_ns()
-    sr.register_transform_stack(data, reference="first", axis=0)
+    sr.register_transform_stack(data[:n_frames], reference="first", axis=0)
     stackreg_time = n_frames / ((time.time_ns() - timer) * 1e-9)  # fps
 
-    times.append((pygpureg_time, stackreg_time))
+    times.append((pygpureg_templated_time, pygpureg_paired_time, stackreg_time))
 
 times = np.asarray(times)
-plt.plot(sizes, times[:, 0], label="pyGPUreg", color=(0.1, 0.5, 0.1, 1.0), linewidth=1, marker='o', markersize=10)
-plt.plot(sizes, times[:, 1], label="StackReg", color=(0.1, 0.1, 0.5, 1.0), linewidth=1, marker='o', markersize=10)
+plt.subplot(1,2,1)
+plt.title("Processing speed of pyGPUreg vs. StackReg")
+plt.plot(sizes, times[:, 0], label="pyGPUreg templated", color=(0.1, 0.5, 0.1, 1.0), linewidth=1, marker='o', markersize=5)
+plt.plot(sizes, times[:, 1], label="pyGPUreg paired", color=(0.5, 0.1, 0.1, 1.0), linewidth=1, marker='o', markersize=5)
+plt.plot(sizes, times[:, 2], label="StackReg", color=(0.1, 0.1, 0.5, 1.0), linewidth=1, marker='o', markersize=5)
 plt.legend()
 plt.xlabel("Image size (pixels)")
 plt.ylabel("Processing speed (fps)")
-plt.ylim([0.0, np.amax(times) * 1.1])
+plt.ylim([-0.1 * np.amax(times), np.amax(times) * 1.1])
+
+plt.subplot(1,2,2)
+plt.title("Speed gain")
+plt.plot(sizes, 100.0 * times[:, 0] / times[:, 2], label="pyGPUreg templated / StackReg", color=(0.1, 0.5, 0.1, 1.0), linewidth=1, marker='o', markersize=5)
+plt.plot(sizes, 100.0 * times[:, 1] / times[:, 2], label="pyGPUreg paired / StackReg", color=(0.5, 0.1, 0.1, 1.0), linewidth=1, marker='o', markersize=5)
+plt.xlabel("Image size (pixels)")
+plt.ylabel("% of StackReg processing speed")
+plt.legend()
 plt.show()
 
